8bea63bbc1f4a7e694546e942d1a406349052eb0
RivasAlejandro <joalrivashe@ittepic.edu.mx>
2017-05-02 09:41:17 -0600

    Proyecto

--- Camera/src/pages/home/home.ts
+++ Camera/src/pages/home/home.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera } from 'ionic-native';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public imageData: string;
+  @Input('useURI') useURI: Boolean = true;
 
   constructor(public navCtrl: NavController) {
 
   }
+
+
+
+
+  getPicture(sourceType) {
+    Camera
+
+  }
+
+
+
+
+
 
 }
